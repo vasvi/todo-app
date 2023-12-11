382fc6b1b70abf8e13121c9e85b189bc21474aac
Verchika Gupta <verchikagupta@gmail.com>
2023-12-11 18:52:11 +0530

    Integrate Add Todo page and make header dynamic

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,12 +2,13 @@ import styles from './layout.module.css';
 import Link from 'next/link';
 
 
-export default function Layout({children}) {
+export default function Layout({children, headerName, showButton}) {
   return (
     <div className={styles.body}>
       <div className={styles.container}>
-        <h1 className={styles.header}>My Todos</h1>
-        <Link className={styles.link} href="\addTodo"> Add new</Link>
+        <h1 className={styles.header}>{headerName}</h1>
+        {showButton? <Link className={styles.link} href="/addTodo"> Add </Link> :
+         <Link className={styles.link} href="/list"> Back </Link>}
       </div>
       {children}
     </div>
