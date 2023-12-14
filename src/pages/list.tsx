a7f8032887c757765f1fde632f7dbb113c4bb44d
Verchika Gupta <verchikagupta@gmail.com>
2023-12-14 12:52:25 +0530

    add add-todo

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -1,14 +1,20 @@
 import { useGetTodos } from "@/hooks/useGetTodos";
 import Layout from '../components/layout';
+//import { useQueryClient } from "react-query";
+import Link from 'next/link';
 
 const List = () => {
     const {todos} = useGetTodos();
+    //const queryClient = useQueryClient()
+    // const queryData = queryClient.getQueryData('todos');
+    // console.log(queryData)
 
     return(
     <Layout headerName="Todo List" showButton={true}>
     <ul>
         {todos?.map((todo: any) => {
-            return <li className="p-2.5" key={todo.id}>{todo.todo}</li>
+            return <span key={todo.id} className="flex"><li className="p-2.5" key={todo.id}>{todo.todo}</li>
+            <Link href={`/${todo.id}`} className="underline border-stone-400 p-2.5">Edit</Link></span>
         })}
         </ul>
     </Layout>    
