adff1096edfd4d2164999d346110e6ed3f74af7f
Vasvi Chawla <vasvi31@gmail.com>
2023-12-15 11:52:31 +0530

    Integrates delete functionality

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -1,18 +1,26 @@
 import { useGetTodos } from "@/hooks/useGetTodos";
+import { useDeleteTodo } from "@/hooks/useDeleteTodo";
+
 import Layout from '../components/layout';
 
 const List = () => {
     const {todos} = useGetTodos();
 
-    return(
-    <Layout headerName="Todo List" showButton={true}>
-    <ul>
-        {todos?.map((todo: any) => {
-            return <li className="p-2.5" key={todo.id}>{todo.todo}</li>
-        })}
-        </ul>
-    </Layout>    
-        );
+    const {mutate} = useDeleteTodo();
+
+    return (
+        <Layout headerName="Todo List" showButton={true}>
+            <ul>
+                {todos?.map((todo: any) => {
+                    return <>
+                        <li className="p-2.5" key={todo.id}>{todo.todo}</li>
+                        <button onClick={()=>mutate(todo.id)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                            Delete</button>
+                        </>
+                })}
+            </ul>
+        </Layout>    
+    );
 }
 
 export default List;
